241450a7092a62854086309bdc3a3e5354c81dce
ricsin19 <sinha.richa19@gmail.com>
2014-08-18 19:19:56 -0700

    Food Receipt Code Added

---
+++ FoodReceipt/SelectDonor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FoodReceipt
+{
+    public partial class SelectDonor : Form
+    {
+        public SelectDonor()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
